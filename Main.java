a49c7296828cecda6c6c42be76b10cfbc8077af1
fahad <fahadkhalf@gmail.com>
2023-09-19 21:17:45 +0300

    scond commit

--- Main.java
+++ Main.java
@@ -1,10 +1,9 @@
 public class Main {
     public static void main(String[] args) {
 
-
-/*------------------some data gast to test the system------------------------*/
-        User.userList.add(new Admin("a","1"));
-        User.userList.add(new Coustomer("c","1"));
+        /*------------------some data gast to test the system------------------------*/
+        User.userList.add(new Admin("a", "1"));
+        User.userList.add(new Coustomer("c", "1"));
 
         Item.addNewItem("apple", 0.3);
         Item.addNewItem("milk ", 9.0);
@@ -12,9 +11,7 @@ public class Main {
         Item.addNewItem("oring", 0.5);
         Item.addNewItem("water", 0.5);
         Item.addNewItem("Chips", 3.0);
-/*----------------------------------------------------------------------------*/
-
-
+        /*----------------------------------------------------------------------------*/
 
         Menu.mainMenu();
     }
